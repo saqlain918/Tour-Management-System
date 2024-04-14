d573b36cb5f07ce0924f77f131ce3ee02702225b
F219244 <100992109+F219244@users.noreply.github.com>
2024-04-14 20:04:05 +0500

    updated Code

--- index.js
+++ index.js
@@ -1,55 +1,50 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = 8000;
-const bodyParser = require('body-parser');
-// Parse application/json
-app.use(bodyParser.json());
-
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+const port = 3000; // Change the port number to 3000
 
 // Set EJS as the view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Set path to your public directory
-const staticPath = path.join(__dirname, "public");
+const staticPath = path.join(__dirname, 'public');
 app.use(express.static(staticPath));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define route for "/"
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // Render the index.ejs file
-  res.render("index");
+  res.render('index');
 });
 
 // Define route for "/signin"
-app.get("/signin", (req, res) => {
+app.get('/signin', (req, res) => {
   // Render the signin.ejs file
-  res.render("signin");
+  res.render('signin');
 });
-// Define route for "/signup"
-app.get("/signup", (req, res) => {
-  // Render the signin.ejs file
-  res.render("signup");
-});
-app.post('/signup', (req, res) => {
-  // Extract form data from request
-  const { first_name, last_name, email, password, gender } = req.body;
 
-  // Here you can add validation, database operations, etc.
+// Define route for "/forget"
+app.get('/forget', (req, res) => {
+  // Render the forgotPassword.ejs file
+  res.render('forget');
+});
+
+app.post("/forget", (req, res) => {
+  // Extract form data from request
+  const email = req.body.email;
+  
 
   // Log the form data
   console.log("Form data received:");
-  console.log("First name:", first_name);
-  console.log("Last name:", last_name);
   console.log("Email:", email);
-  console.log("Password:", password);
-  console.log("Gender:", gender);
+ 
 
   // Send response
-  res.send("Form data received successfully!");
+  res.send("forget data received successfully!");
 });
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
